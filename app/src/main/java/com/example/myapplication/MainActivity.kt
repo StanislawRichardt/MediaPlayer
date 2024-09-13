d0d173712aad369908a6e042065e520c465083ec
Stanisław Richardt <stanislaw.richardt@uni.opole.pl>
2024-09-13 11:01:15 +0200

    ViewModel init refactoring

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.ui.eventscreen.EventListViewModel
 import com.example.myapplication.ui.navigation.PortfolioApp
@@ -17,12 +20,11 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
 class MainActivity : ComponentActivity() {
-    private lateinit var eventListViewModel: EventListViewModel
-    private lateinit var scheduleListViewModel: ScheduleListViewModel
+    private val eventListViewModel: EventListViewModel by viewModels()
+    private val scheduleListViewModel: ScheduleListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        eventListViewModel = ViewModelProvider(this, EventListViewModelFactory())[EventListViewModel::class.java]
-        scheduleListViewModel = ViewModelProvider(this, ScheduleListViewModelFactory())[ScheduleListViewModel::class.java]
         setContent {
             MyApplicationTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
